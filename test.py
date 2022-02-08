fdedf9ac865ca2a1962239ab10f98858f2987b23
TANGYISHUN <twogod@live.com>
2022-02-08 22:06:32 +0800

    save

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
+import pyautogui
+
 if __name__ == '__main__':
-    a = b = c = []
-    c.append([5,10,20,50])
-    b.append([100,200,500,1440])
-    print(a)
+    im = pyautogui.locateCenterOnScreen('D:/03_HUAWEI/Code/01_Arknights_AutoPlay/pic/start.png', confidence=0.9)
+    print(im)
